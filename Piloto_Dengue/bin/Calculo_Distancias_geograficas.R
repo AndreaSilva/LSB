58f106f1760d5e99dae0e0c1732435613634eb39
AndreaSilva <andreasilvacala@gmail.com>
2018-03-31 23:34:14 +0200

    paso comentarios a ingles

--- Piloto_Dengue/bin/Calculo_Distancias_geograficas.R
+++ Piloto_Dengue/bin/Calculo_Distancias_geograficas.R
@@ -1,10 +1,14 @@
-# Distancias Geograficas
-# point distances
+#############################################################################
+####            Euclidean geographical distance calculation           #######
+#####                  Raster -- point distances                      #######
+#############################################################################
 
+# Required library
 library(raster)
 
-## GEN E ##
-#1. Gen E con localidad
+## E GEN ##
+
+# Gen E -- locality: Data of E gene with reported location
 CoordenadasGE_loan <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genE/sub_database/Coordenadas_loan.csv")
 lon_lat <- CoordenadasGE_loan[,c(4,3)]
 
@@ -13,44 +17,9 @@ colnames(dist.GE.loan) <- CoordenadasGE_loan$Location
 rownames(dist.GE.loan) <- CoordenadasGE_loan$Location
 write.csv(dist.GE.loan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genE/matrices_geograficas/dist.GE.loan.csv")
 
-#1.1 Gen E Dengue 1 localidades
-CoordenadasD1GE <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genE/sub_database/CoordenadasD1_loan.csv")
-lon_lat1 <- CoordenadasD1GE[,c(4,3)]
-
-dist.D1.loan <- pointDistance(lon_lat1, lon_lat1, lonlat = F, allpairs = T)
-colnames(dist.D1.loan) <- CoordenadasD1GE$Location
-rownames(dist.D1.loan) <- CoordenadasD1GE$Location
-write.csv(dist.D1.loan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genE/matrices_geograficas/dist.D1.loan.csv")
-
-#1.2 Gen E Dengue 2 localidades
-CoordenadasD2GE <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genE/sub_database/CoordenadasD2_loan.csv")
-lon_lat2 <- CoordenadasD2GE[,c(4,3)]
-
-dist.D2.loan <- pointDistance(lon_lat2, lon_lat2, lonlat = F, allpairs = T)
-colnames(dist.D2.loan) <- CoordenadasD2GE$Location
-rownames(dist.D2.loan) <- CoordenadasD2GE$Location
-write.csv(dist.D2.loan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genE/matrices_geograficas/dist.D2.loan.csv")
-
-#1.3 Gen E Dengue 3 localidades
-CoordenadasD3GE <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genE/sub_database/CoordenadasD3_loan.csv")
-lon_lat3 <- CoordenadasD3GE[,c(4,3)]
-
-dist.D3.loan <- pointDistance(lon_lat3, lon_lat3, lonlat = F, allpairs = T)
-colnames(dist.D3.loan) <- CoordenadasD3GE$Location
-rownames(dist.D3.loan) <- CoordenadasD3GE$Location
-write.csv(dist.D3.loan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genE/matrices_geograficas/dist.D3.loan.csv")
-
-#1.4 Gen E Dengue 4 localidades
-CoordenadasD4GE <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genE/sub_database/CoordenadasD4_loan.csv")
-lon_lat4 <- CoordenadasD4GE[,c(4,3)]
-
-dist.D4.loan <- pointDistance(lon_lat4, lon_lat4, lonlat = F, allpairs = T)
-colnames(dist.D4.loan) <- CoordenadasD4GE$Location
-rownames(dist.D4.loan) <- CoordenadasD4GE$Location
-write.csv(dist.D4.loan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genE/matrices_geograficas/dist.D4.loan.csv")
 #--------------------------------
 
-#1. Gen E con paises
+# E Gen -- Country: Data of E gene with reported country
 CoordenadasGE_loan <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genE/sub_database/Coordenadas_paan.csv")
 lon_lat <- CoordenadasGE_loan[,c(3,4)]
 
@@ -59,45 +28,9 @@ colnames(dist.GE.loan) <- CoordenadasGE_loan$Country
 rownames(dist.GE.loan) <- CoordenadasGE_loan$Country
 write.csv(dist.GE.loan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genE/matrices_geograficas/dist.GE.paan.csv")
 
-#1.1 Gen E Dengue 1 paises
-CoordenadasD1GE <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genE/sub_database/CoordenadasD1_paan.csv")
-lon_lat1 <- CoordenadasD1GE[,c(3,4)]
+## GENOME ##
 
-dist.D1.loan <- pointDistance(lon_lat1, lon_lat1, lonlat = F, allpairs = T)
-colnames(dist.D1.loan) <- CoordenadasD1GE$Country
-rownames(dist.D1.loan) <- CoordenadasD1GE$Country
-write.csv(dist.D1.loan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genE/matrices_geograficas/dist_D1_paan.csv")
-
-#1.2 Gen E Dengue 2 paises
-CoordenadasD2GE <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genE/sub_database/CoordenadasD2_paan.csv")
-lon_lat2 <- CoordenadasD2GE[,c(3,4)]
-
-dist.D2.loan <- pointDistance(lon_lat2, lon_lat2, lonlat = F, allpairs = T)
-colnames(dist.D2.loan) <- CoordenadasD2GE$Country
-rownames(dist.D2.loan) <- CoordenadasD2GE$Country
-write.csv(dist.D2.loan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genE/matrices_geograficas/dist_D2_paan.csv")
-
-#1.3 Gen E Dengue 3 paises
-CoordenadasD3GE <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genE/sub_database/CoordenadasD3_paan.csv")
-lon_lat3 <- CoordenadasD3GE[,c(3,4)]
-
-dist.D3.loan <- pointDistance(lon_lat3, lon_lat3, lonlat = F, allpairs = T)
-colnames(dist.D3.loan) <- CoordenadasD3GE$Country
-rownames(dist.D3.loan) <- CoordenadasD3GE$Country
-write.csv(dist.D3.loan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genE/matrices_geograficas/dist_D3_paan.csv")
-
-#1.4 Gen E Dengue 4 paises
-CoordenadasD4GE <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genE/sub_database/CoordenadasD4_paan.csv")
-lon_lat4 <- CoordenadasD4GE[,c(3,4)]
-
-dist.D4.loan <- pointDistance(lon_lat4, lon_lat4, lonlat = F, allpairs = T)
-colnames(dist.D4.loan) <- CoordenadasD4GE$Country
-rownames(dist.D4.loan) <- CoordenadasD4GE$Country
-write.csv(dist.D4.loan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genE/matrices_geograficas/dist_D4_paan.csv")
-
-## GENOMA ##
-
-#2. Genoma con localidad
+# Genome -- locality: Data of genome with reported location
 CoordenadasGN_loan <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genoma_completo/sub_database/Coordenadas_loan.csv")
 lon_lat <- CoordenadasGN_loan[,c(4,3)]
 
@@ -106,43 +39,9 @@ colnames(dist.GN.loan) <- CoordenadasGN_loan$Location
 rownames(dist.GN.loan) <- CoordenadasGN_loan$Location
 write.csv(dist.GN.loan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genoma/matrices_geograficas/dist.GN.loan.csv")
 
-#2.1 Genoma dengue 1 con localidad
-CoordenadasD1_loan <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genoma_completo/sub_database/CoordenadasD1_loan.csv")
-lon_lat1 <- CoordenadasD1_loan[,c(4,3)]
-
-dist.D1.loan <- pointDistance(lon_lat1, lon_lat1, lonlat = F, allpairs = T)
-colnames(dist.D1.loan) <- CoordenadasD1_loan$Location
-rownames(dist.D1.loan) <- CoordenadasD1_loan$Location
-write.csv(dist.D1.loan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genoma/matrices_geograficas/dist.D1.loan.csv")
-
-#2.2 Genoma dengue 2 con localidad
-CoordenadasD2_loan <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genoma_completo/sub_database/CoordenadasD2_loan.csv")
-lon_lat2 <- CoordenadasD2_loan[,c(4,3)]
-
-dist.D2.loan <- pointDistance(lon_lat2, lon_lat2, lonlat = F, allpairs = T)#REVISAR
-colnames(dist.D2.loan) <- CoordenadasD2_loan$Location
-rownames(dist.D2.loan) <- CoordenadasD2_loan$Location
-write.csv(dist.D2.loan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genoma/matrices_geograficas/dist.D2.loan.csv")
-
-#2.3 Genoma dengue 3 con localidad
-CoordenadasD3_loan <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genoma_completo/sub_database/CoordenadasD3_loan.csv")
-lon_lat3 <- CoordenadasD3_loan[,c(4,3)]
-
-dist.D3.loan <- pointDistance(lon_lat3, lon_lat3, lonlat = F, allpairs = T)
-colnames(dist.D3.loan) <- CoordenadasD3_loan$Location
-rownames(dist.D3.loan) <- CoordenadasD3_loan$Location
-write.csv(dist.D3.loan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genoma/matrices_geograficas/dist.D3.loan.csv")
-
-#2.4 Genoma dengue 4 con localidad
-CoordenadasD4_loan <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genoma_completo/sub_database/CoordenadasD4_loan.csv")
-lon_lat4 <- CoordenadasD4_loan[,c(4,3)]
-
-dist.D4.loan <- pointDistance(lon_lat4, lon_lat4, lonlat = F, allpairs = T)
-colnames(dist.D4.loan) <- CoordenadasD4_loan$Location
-rownames(dist.D4.loan) <- CoordenadasD4_loan$Location
-write.csv(dist.D4.loan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genoma/matrices_geograficas/dist.D4.loan.csv")
 #-----------------------
-#3. Genoma con pais
+
+# Genome -- country: Data of genome with reported country
 
 CoordenadasGN_paan <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genoma_completo/sub_database/Coordenadas_paan.csv")
 lon_lat <- CoordenadasGN_paan[,c(4,3)]
@@ -151,39 +50,3 @@ dist.GN.paan <- pointDistance(lon_lat, lon_lat, lonlat = F, allpairs = T)
 colnames(dist.GN.paan) <- CoordenadasGN_paan$Country
 rownames(dist.GN.paan) <- CoordenadasGN_paan$Country
 write.csv(dist.GN.paan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genoma/matrices_geograficas/dist.GN.paan.csv")
-
-#3.1 Genoma dengue 1 con pais
-CoordenadasD1_paan <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genoma_completo/sub_database/CoordenadasD1_paan.csv")
-lon_lat1 <- CoordenadasD1_paan[,c(4,3)]
-
-dist.D1.paan <- pointDistance(lon_lat1, lon_lat1, lonlat = F, allpairs = T)
-colnames(dist.D1.paan) <- CoordenadasD1_paan$Country
-rownames(dist.D1.paan) <- CoordenadasD1_paan$Country
-write.csv(dist.D1.paan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genoma/matrices_geograficas/dist.D1.paan.csv")
-
-#3.2 Genoma dengue 2 con pais
-CoordenadasD2_paan <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genoma_completo/sub_database/CoordenadasD2_paan.csv")
-lon_lat2 <- CoordenadasD2_paan[,c(4,3)]
-
-dist.D2.paan <- pointDistance(lon_lat2, lon_lat2, lonlat = F, allpairs = T)
-colnames(dist.D2.paan) <- CoordenadasD2_paan$Country
-rownames(dist.D2.paan) <- CoordenadasD2_paan$Country
-write.csv(dist.D2.paan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genoma/matrices_geograficas/dist.D2.paan.csv")
-
-#3.3 Genoma dengue 3 con pais
-CoordenadasD3_paan <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genoma_completo/sub_database/CoordenadasD3_paan.csv")
-lon_lat3 <- CoordenadasD3_paan[,c(4,3)]
-
-dist.D3.paan <- pointDistance(lon_lat3, lon_lat3, lonlat = F, allpairs = T)
-colnames(dist.D3.paan) <- CoordenadasD3_paan$Country
-rownames(dist.D3.paan) <- CoordenadasD3_paan$Country
-write.csv(dist.D3.paan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genoma/matrices_geograficas/dist.D3.paan.csv")
-
-#3.4 Genoma dengue 4 con pais
-CoordenadasD4_paan <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genoma_completo/sub_database/CoordenadasD4_paan.csv")
-lon_lat4 <- CoordenadasD4_paan[,c(4,3)]
-
-dist.D4.paan <- pointDistance(lon_lat4, lon_lat4, lonlat = F, allpairs = T)
-colnames(dist.D4.paan) <- CoordenadasD4_paan$Country
-rownames(dist.D4.paan) <- CoordenadasD4_paan$Country
-write.csv(dist.D4.paan,file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/matrices_genoma/matrices_geograficas/dist.D4.paan.csv")
