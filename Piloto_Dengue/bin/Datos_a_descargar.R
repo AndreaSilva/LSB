c927a5420dd8d4129316e76a7c373d4b1c90379f
AndreaSilva <andreasilvacala@gmail.com>
2018-04-02 23:17:50 +0200

    paso comentarios a ingles

--- Piloto_Dengue/bin/Datos_a_descargar.R
+++ Piloto_Dengue/bin/Datos_a_descargar.R
@@ -1,47 +1,46 @@
 ################################################################################
-####              Descargar secuencias de Genoma completo                #######
-###                y del Gen E del virus del Dengue a partir             #######
-###                          del numero de acceso                       ########
+####                      Download complete Genome                       #######
+###              and of the E gene sequences of the Dengue virus         #######
+###                        from the Accession Number                     #######
 ################################################################################
 
-## DESCARGAR BASE DE DATOS DE GENOMA COMPLETO ###
+## Download complete Genome Sequences ###
 
-# Leer el archivo con la base de datos 
+# Read file with the database
 bd_dengue <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/data/Base_Datos/Base_Datos_Dengue/bd_Dengue.csv", stringsAsFactors = F)
 
-# Saber las posiciones de los datos con las siguentes caracteristicas:
-# (Esto con el fin de poder eliminar estos datos que no me sirven para mi trabajo y poder descargar los datos ya filtrados)
+# Data positions with the following characteristics (to eliminate them):
 
-# 1. Los datos que tengan NA tanto en la columna pais como en la columna ano (En las dos al mismo tiempo)
+# 1. Data with NA in the "country" column and the "year" column (In both at the same time)
 country_year_na <- which(is.na(bd_dengue$Country) & is.na(bd_dengue$Year))
 
-# 2. Los datos que no tienen reportado el serotipo
+# 2. Data with unknown serotype
 dsserotype <- which(bd_dengue$Serotype=="DENV")
 
-# 3. Los datos que son clones
+# 3. Data that are clones
 dsclone <- which(bd_dengue$Serotype=="Clone")
 
-# 4. Los datos no verificados
+# 4. Unverified data
 dsunverified <- which(bd_dengue$Serotype=="UNVERIFIED")
 
-# 5. Los datos que son quimeras
+# 5. Data that are chimeras
 dschimeric <- which(bd_dengue$Serotype=="Chimeric")
 
-# Concateno las posiciones de esos datos que no me sirve, que no quiero descargar
+# Join data positions to eliminate
 bd_eliminar <- c(country_year_na,dsserotype,dschimeric, dsclone,dsunverified)
 
 bd_eliminar <- unique(bd_eliminar)
 
-# Eliminos esos datos que ya he identificado antes y que no quiero descargar
+# Delete data that is not downloaded
 datos <- bd_dengue[-bd_eliminar,]
 
-# Escribo esta nueva version de la base de datos
+# New data base with E gene and genome data
 write.csv(datos,file = "/home/andrea/LSB/Piloto_Dengue/data/Base_Datos/Base_Datos_Dengue/bd_Dengue_v02.csv")
 
-# De la nueva base de datos Extraigo solo los datos que tiene genoma completo
+# From the new database: Extract data from the complete genome
 Complete_genome <- datos[which(datos$Gene=="Complete_Genome" & datos$Size_sequence>10100),]
 
-# Lista de numeros de acceso
+# List of Accession Aumbers
 ID <- Complete_genome$N_Accesion
 
 which(ID=="AJ487271")
@@ -49,38 +48,38 @@ No.Accesion <-ID[3893:3916]
 ID[3893]
 length(ID)
 ----------------------------------------------------------------------------------------------------
-# Estas secuencias fueron descargadas usando la funcion escrita por Viviana Romero-Alarcon 
-# en el codigo downloadCDSgb.R 
-# ejecutado con el codigo CDS_dowland_genbank.R 
-# https://github.com/alarconvv/download.CDS.GenBank  
-  
-  
-#Algunas secuencias no se descargaron por alguna de las sifuientes razones:
+# These sequences were downloaded using the function written by Viviana Romero-Alarcon
+# in the code downloadCDSgb.R
+# executed with the code CDS_dowland_genbank.R
+# https://github.com/alarconvv/download.CDS.GenBank
 
-# No se reporta el CDS en el GenBank
-# No se reporta un numero de acceso, solo se reporta el numero gb y este numero no esreconocido por el código
-# Tiene una longitud menor a 1100 pb
-# Estan etiquetadas por el Gen Bnak como clon, quimera, inverificada
-# No reporta el serotipo al que pertenece
 
-# No tiene como tal un numero de acceso si no que se reporta se numero de gb asi:
-#  "NC_002640" "NC_001474" "NC_001477" "NC_001475" 
+#Some sequences were not downloaded for the following reasons:
 
-# NO REPORTA CDS
-#sincds <- c("EU920839", "KC131141", "KC131140", "HM631854", "HM631853" ,"HM631852", "HM631851", "HM631855", 
-#            "FJ913015", "JQ045685", "JQ045683", "JQ045682", "JQ045681", "JQ045680", "JQ045679", "JQ045678", 
-#            "JQ045677", "JQ045676", "JQ045675", "JQ045674", "JQ045673", "JQ045672", "JQ045671", "JQ045669", 
+# The CDS is not reported in GenBank
+# An access number is not reported, only the gb number is reported and this number is not recognized by the code
+# It has a length less than 1100 bp
+# They are labeled by GenBank as a clone, chimera, unverified
+# Does not report the serotype
+
+# only the gb number is reported:
+#  "NC_002640" "NC_001474" "NC_001477" "NC_001475"
+
+# The CDS is not reported in GenBank
+#sincds <- c("EU920839", "KC131141", "KC131140", "HM631854", "HM631853" ,"HM631852", "HM631851", "HM631855",
+#            "FJ913015", "JQ045685", "JQ045683", "JQ045682", "JQ045681", "JQ045680", "JQ045679", "JQ045678",
+#            "JQ045677", "JQ045676", "JQ045675", "JQ045674", "JQ045673", "JQ045672", "JQ045671", "JQ045669",
 #            "JQ045627", "M87512", "EU179861", "EU179860", "FJ177308")
 
-# Clones
+# Clone
 # FJ828987, FJ828986, AY145122, AY145121, AY145123, U88537, U88536, U88535, AB543624
 # AF375822, AF326827, AF326826, AF326825, DQ285561,
 ----------------------------------------------------------------------------------------------------
-# Patición para correr por partes. 
+# Partition to run by parts.
 
 ID <- ID[1:5]
-ID <- ID[501:1000]  
-ID <- ID[1001:1500] 
+ID <- ID[501:1000]
+ID <- ID[1001:1500]
 ID <- ID[1501:2000]
 ID <- ID[2001:2500]
 ID <- ID[2501:3000]
@@ -93,16 +92,16 @@ sequences_dengue <- read.csv(file = "/home/andrea/LSB/Piloto_Dengue/bin/descarga
 #-----------------------------------------------------------------------------------------
 
 ###################################
-# Descargar secuencias del gen E  #
+# Download E gene sequences  #
 ####################################
 
-# Librerias requeridas
+# Required libraries
 
 library(ape)
 library(seqinr)
 
 # Extraigo solo los datos de gen E, que tengan una longitud de la secuencia entre
-# 1300 a 1600 pb , teniendo encuenta que la longitud del gen E reportada por las 
+# 1300 a 1600 pb , teniendo encuenta que la longitud del gen E reportada por las
 # secuencias de referencia del GenBnak es de 1485 pb
 
 Gen_E <- datos[datos$Gene=="E" & datos$Size_sequence>1300 & datos$Size_sequence<1600, ]
@@ -118,6 +117,6 @@ genE_sequences <- read.GenBank(ids)
 
 genE_sequences_ids <- paste(attr(genE_sequences, "species"), names(genE_sequences), sep = "_")
 
-# Guardando las secuencias 
+# Guardando las secuencias
 
 write.dna(genE_sequences,"/home/andrea/LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genE/Gen_E.fasta", format = "fasta")
